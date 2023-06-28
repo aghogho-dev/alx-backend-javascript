ad70527ea2271adb03aa0eaeba1d56704f17c7c8
Aghogho <monorienaghogho@gmail.com>
2023-06-28 22:59:24 +0000

    9. ES6 methods

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,15 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    income: budget.income,
+    gdp: budget.gdp,
+    capita: budget.capita,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+
+  };
+
+  return fullBudget;
+}

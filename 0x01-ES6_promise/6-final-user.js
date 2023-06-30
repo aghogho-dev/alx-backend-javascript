bde01179078eada97a979bbd44f4a348e415b16c
Aghogho <monorienaghogho@gmail.com>
2023-06-30 00:37:12 +0000

    6. handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const results = await Promise.all(promises);
-  const response = results.map((result) => {
-    const status = result.status;
-    const value = status === 'fulfilled' ? result.value : String(result.reason);
-    return {
-      status,
-      value,
-    };
-  });
-  return Promise.resolve(response);
+  try {
+    const [userSignupResult, photoUploadResult] = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName)
+    ]);
+
+    return userSignupResult.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason)
+    }));
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }

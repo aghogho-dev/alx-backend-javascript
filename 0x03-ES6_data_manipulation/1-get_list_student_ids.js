67366d289725b0b3053638651fd0a4040b8f6df4
Aghogho <monorienaghogho@gmail.com>
2023-07-06 02:09:40 +0000

    1. more map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,8 @@
+/**
+ * Returns id of arr
+ * @param {{ arr }}
+ * @returns {{ arr }}
+ */
 function getListStudentIds(arr) {
   if (!(arr instanceof Array)) return [];
   return arr.map((x) => x.id);
